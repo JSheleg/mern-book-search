0fce2153b52ee1bb52cccb208d58fa7fff05df56
JSheleg <jessica.sheleg@gmail.com>
2021-10-07 18:53:09 -0500

    updated npm install

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+//db connection
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas')
+//import authentication middleware
+const {authMiddleWare} = require('./utils/auth');
+const { Server } = require('http');
+// const routes = require('./routes');
 
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//apollo server with express app
+Server.applyMiddleWare({app});
+
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +26,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+//get all 
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
 });
