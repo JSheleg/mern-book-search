6a4d3e162a0ceae0ceb8c6bdaf528c6127c9184a
JSheleg <jessica.sheleg@gmail.com>
2021-10-07 19:12:30 -0500

    update auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,18 +5,28 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
+    //if no token, return requset object as is
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req
     }
 
     // verify token and get user data out of it
@@ -28,12 +38,8 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+    // return updated request object
+    return req;
+  }
+  
 };
