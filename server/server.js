08d9bad9ff2d18246fc1e71c1774bb26bba58f70
JSheleg <jessica.sheleg@gmail.com>
2021-10-08 14:21:13 -0500

    resolvers and debugging

--- server/server.js
+++ server/server.js
@@ -42,5 +42,8 @@ app.get('*', (req,res) => {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
